0c6dd04a383bf53ffe242f61db62218ecaadecf0
Fabien Chouteau <fabien.chouteau@gmail.com>
2022-05-01 12:30:14 +0200

    Export/Import prototype

--- pico_hub_nodes.js
+++ pico_hub_nodes.js
@@ -1,6 +1,5 @@
-// Remove built-in node...
+// Remove built-in nodes...
 LiteGraph.registered_node_types = {}
-
 
 LGraphNode.prototype.addCableInput = function (label) {
     this.addInput(label, "MIDI_Cable", {shape: LiteGraph.BOX_SHAPE})
@@ -90,11 +89,12 @@ function NodeCableMixer() {
     this.addCableInput("In");
     this.addCableInput("In");
     this.addCableOutput("Out");
+    this.properties = { number_of_inputs: 2 };
     this.addWidget("button","+",null,function(){
             that.addCableInput("In");
+            that.properties.number_of_inputs += 1;
         });
 }
-
 NodeCableMixer.title = "Cable Mixer";
 NodeCableMixer.desc = "Mix messages from different cable in a single one";
 LiteGraph.registerNodeType("cable/mixer", NodeCableMixer);
@@ -105,8 +105,10 @@ function NodeChannelMixer() {
     this.addChannelInput("In");
     this.addChannelInput("In");
     this.addChannelOutput("Out");
+    this.properties = { number_of_inputs: 2 };
     this.addWidget("button","+",null,function(){
             that.addChannelInput("In");
+            that.properties.number_of_inputs += 1;
         });
 }
 
@@ -126,9 +128,9 @@ LiteGraph.registerNodeType("message/on_note", NodeOnNoteMessage);
 
 function NodeFilterChannel()
 {
-    this.addCableInput("MIDI");
+    this.addCableInput("Cable");
     this.addChannelOutput("Channel");
-    this.addCableOutput("MIDI");
+    this.addCableOutput("Cable");
     this.properties = { channel_id : 1 };
     this.combo_widget = this.addWidget("combo", "Channel ID", 1, null,
                                        { values:MIDI_Chan_Ids(),
@@ -140,8 +142,8 @@ LiteGraph.registerNodeType("channel/filter", NodeFilterChannel);
 function NodeMergeChannel()
 {
     this.addChannelInput("Channel");
-    this.addCableInput("MIDI");
-    this.addCableOutput("MIDI");
+    this.addCableInput("Cable");
+    this.addCableOutput("Cable");
 }
 NodeMergeChannel.title = "MergeChannel";
 LiteGraph.registerNodeType("channel/merge", NodeMergeChannel);
@@ -174,3 +176,101 @@ function NodeTest()
 }
 NodeTest.title = "Test Node";
 LiteGraph.registerNodeType("pico_hub/test", NodeTest);
+
+
+LGraph.prototype.toPicoHubConfig = function () {
+    const data = this.serialize();
+    var text = ""
+
+        for (const node of data.nodes) {
+            text += "node=" + node.type + ":";
+            text += "id=" + node.id + ":";
+            text += "pos_x=" + node.pos[0] + ":";
+            text += "pos_y=" + node.pos[1] + ":";
+
+            if (node.properties) {
+                for (const [key, value] of Object.entries(node.properties)) {
+                    text += key + "=" + value + ":";
+                }
+            }
+            text += "\n";
+        }
+
+    for (const link of data.links) {
+        const id = link[0];
+        const origin_id = link[1];
+        const origin_slot = link[2];
+        const target_id = link[3];
+        const target_slot = link[4];
+        const type = link[5];
+
+        text += "link=" + type + ":";
+        text += origin_id + "-" + origin_slot + ":";
+        text += target_id + "-" + target_slot;
+        text += "\n";
+    }
+
+    return text;
+}
+
+LGraph.prototype.loadPicoHubConfig = function (config) {
+    graph.clear();
+
+    var conf_id_to_graph_id = {}
+
+    for (const line of config.split('\n')) {
+        if (line.startsWith("node")) {
+
+            var node = null;
+            var id = -1;
+
+            for (const elt of line.split(':')) {
+                const [key, value] = elt.split('=');
+
+                if (key == "node") {
+                    node = LiteGraph.createNode(value);
+                } else if (key == "id") {
+                    id = parseInt(value);
+                } else if (key == "pos_x") {
+                    node.pos[0] = parseInt(value);
+                } else if (key == "pos_y") {
+                    node.pos[1] = parseInt(value);
+                } else {
+                    if (node.properties && key in node.properties) {
+                        node.properties[key] = value;
+                    }
+                }
+            }
+
+            if (node.type == "cable/mixer") {
+                var cnt = 2;
+                while (cnt < node.properties.number_of_inputs) {
+                    node.addCableInput("In");
+                    cnt += 1;
+                }
+            } else if (node.type == "channel/mixer") {
+                var cnt = 2;
+                while (cnt < node.properties.number_of_inputs) {
+                    node.addChannelInput("In");
+                    cnt += 1;
+                }
+            }
+
+            graph.add(node);
+            conf_id_to_graph_id[id] = node.id;
+
+        } else if (line.startsWith("link")) {
+            const [type, org, tar] = line.split(':');
+
+            [org_id, org_slot] = org.split('-');
+            [tar_id, tar_slot] = tar.split('-');
+
+            const origin_id = conf_id_to_graph_id[parseInt(org_id)];
+            const target_id = conf_id_to_graph_id[parseInt(tar_id)];
+            var origin = graph.getNodeById(origin_id);
+            var target = graph.getNodeById(target_id);
+
+            origin.connect(parseInt(org_slot), target, parseInt(tar_slot));
+        }
+    }
+}
